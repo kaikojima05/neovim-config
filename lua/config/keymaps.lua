4e209b90e07e4de366f3b9a858b5c334c23b7be6
kaikojima <kojimakai5335@gmail.com>
2024-05-13 20:59:09 +0900

    added keymaps to jump to error and warning locations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,7 +40,7 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 keymap.set("n", "te", ":tabedit")
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 
--- lspsaga
+-- lsp
 keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 keymap.set("n", "lf", "<cmd>Lspsaga lsp_finder<CR>")
 keymap.set("n", "rk", "<cmd>Lspsaga peek_definition<CR>")
@@ -49,3 +49,15 @@ keymap.set("n", "gn", "<cmd>Lspsaga rename<CR>")
 keymap.set("n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>")
 keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap.set(
+  "n",
+  "E",
+  "<cmd>lua vim.diagnostic.goto_prev({severity = { min = vim.diagnostic.severity.ERROR, max = vim.diagnostic.severity.ERROR }})<CR>",
+  { noremap = true, silent = true }
+)
+keymap.set(
+  "n",
+  "W",
+  "<cmd>lua vim.diagnostic.goto_prev({severity = { min = vim.diagnostic.severity.WARNING, max = vim.diagnostic.severity.WARNING }})<CR>",
+  { noremap = true, silent = true }
+)
