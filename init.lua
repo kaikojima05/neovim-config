a38e8111730895fea1cae537ebe9f4eebc9f3ea1
kaikojima <kojimakai5335@gmail.com>
2024-05-29 03:04:16 +0900

    added keymaps to Visual-Studio-Code

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  vim.api.nvim_set_option("clipboard", "unnamedplus")
+  vim.api.nvim_set_keymap("n", "di", 'di"', { noremap = true })
+  vim.api.nvim_set_keymap("n", "di", "di{", { noremap = true })
+  vim.api.nvim_set_keymap("n", "di", "di[", { noremap = true })
+  vim.api.nvim_set_keymap("i", "<C-k>", "<ESC>la", {})
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
