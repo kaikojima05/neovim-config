5cc4e0c3b487fcd33b488be351830e346c7c25e7
kaikojima <kojimakai5335@gmail.com>
2024-05-29 04:30:15 +0900

    restored a keymap for Visual Studio Code

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ if vim.g.vscode then
   vim.api.nvim_set_keymap("n", "di", 'di"', { noremap = true })
   vim.api.nvim_set_keymap("n", "di", "di{", { noremap = true })
   vim.api.nvim_set_keymap("n", "di", "di[", { noremap = true })
-  vim.api.nvim_set_keymap("i", "<C-k>", "<ESC>la", {})
+  vim.api.nvim_set_keymap("i", "kk", "<ESC>la", {})
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
