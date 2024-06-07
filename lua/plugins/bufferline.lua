18684cc4e86b198adc242ac9f0a43834d5aad1d6
kaikojima <kojimakai5335@gmail.com>
2024-06-07 19:38:45 +0900

    changed the tab design of the `bufferline` plugin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,14 +3,10 @@ return {
     "akinsho/bufferline.nvim",
     depends = { "kyazdani42/nvim-web-devicons" },
     event = "VeryLazy",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
     config = function()
       require("bufferline").setup({
         options = {
-          mode = "tabs",
+          mode = "buffers",
           separator_style = "slant",
           show_buffer_close_icons = false,
           show_close_icon = false,
@@ -19,6 +15,10 @@ return {
           guibg = "#181c24",
         },
       })
+
+      -- delete default keymaps
+      vim.api.nvim_del_keymap("n", "H")
+      vim.api.nvim_del_keymap("n", "L")
     end, -- end,
   },
 }
